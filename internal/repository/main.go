6de5dee6047656d532a554043def49c36c8da759
nua <enuesaa@gmail.com>
2024-11-19 20:58:10 +0900

    feat(repos): create log repository

--- internal/repository/main.go
+++ internal/repository/main.go
@@ -3,11 +3,13 @@ package repository
 type Repos struct {
 	Fs  FsRepositoryInterface
 	Cmd CmdRepositoryInterface
+	Log LogRepositoryInterface
 }
 
 func New() Repos {
 	return Repos{
 		Fs: &FsRepository{},
 		Cmd: &CmdRepository{},
+		Log: &LogRepository{},
 	}
 }
