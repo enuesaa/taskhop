2f0543591f24c47ba087892b546adf51f6f0f785
nua <enuesaa@gmail.com>
2024-11-20 23:59:24 +0900

    fix: `type ctxKey struct{}should not use built-in type string as key for value; define your own type to avoid collisions`

--- internal/repository/log.go
+++ internal/repository/log.go
@@ -12,26 +12,27 @@ func init() {
 	slog.SetDefault(logger)
 }
 
+type logAttrKey struct{}
+
 type LogRepositoryInterface interface {
 	Use(ctx context.Context, key string, value interface{}) context.Context
-	// otel 対応をするのであれば ctx をここで渡し span id を取り出すのが良さそう
 	Info(ctx context.Context, format string, a ...any)
 }
 
 type LogRepository struct {}
 
 func (repo *LogRepository) Use(ctx context.Context, key string, value interface{}) context.Context {
-	logattr, ok := ctx.Value("logattr").(map[string]interface{})
+	logattr, ok := ctx.Value(logAttrKey{}).(map[string]interface{})
 	if !ok {
 		logattr = map[string]interface{}{}
 	}
 	logattr[key] = value
 
-	return context.WithValue(ctx, "logattr", logattr)
+	return context.WithValue(ctx, logAttrKey{}, logattr)
 }
 
 func (repo *LogRepository) Info(ctx context.Context, format string, a ...any) {
-	logattr, ok := ctx.Value("logattr").(map[string]interface{})
+	logattr, ok := ctx.Value(logAttrKey{}).(map[string]interface{})
 	if !ok {
 		logattr = map[string]interface{}{}
 	}
