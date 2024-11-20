fca1db17b6510310a327642d60f5b11977c3c75f
nua <enuesaa@gmail.com>
2024-11-20 20:24:30 +0900

    feat(http): use httplog middleware. このプロジェクトではログのフォーマットを慎重に決めたいのでその準備として、chi でログフォーマットを調整できることを確認した

--- internal/repository/log.go
+++ internal/repository/log.go
@@ -9,6 +9,10 @@ func init() {
 	log.SetFlags(0)
 }
 
+// TODO
+// - https://qiita.com/Imamotty/items/3fbe8ce6da4f1a653fae
+// - https://medium.com/arigatobank-tech-blog/go-1-21-slog-5f0d9804204f
+
 type LogRepositoryInterface interface {
 	Printf(format string, a ...any)
 	Fatalf(format string, a ...any)
--- internal/router/main.go
+++ internal/router/main.go
@@ -1,6 +1,9 @@
 package router
 
 import (
+	"log/slog"
+	"time"
+
 	"github.com/enuesaa/taskhop/internal/repository"
 	"github.com/enuesaa/taskhop/internal/routegql"
 	"github.com/enuesaa/taskhop/internal/routegqlplayground"
@@ -9,13 +12,22 @@ import (
 	"github.com/go-chi/chi/v5"
 	"github.com/go-chi/chi/v5/middleware"
 	"github.com/go-chi/cors"
+	"github.com/go-chi/httplog/v2"
 )
 
 func New(repos repository.Repos) *chi.Mux {
 	app := chi.NewRouter()
 
 	// middleware
-	app.Use(middleware.Logger)
+	logger := httplog.NewLogger("http", httplog.Options{
+		LogLevel:        slog.LevelDebug,
+		TimeFieldFormat: time.RFC3339,
+		// Concise:          true,
+		// RequestHeaders:   true,
+	})
+	app.Use(httplog.RequestLogger(logger))
+	app.Use(middleware.Recoverer)
+	app.Use(middleware.NoCache)
 	app.Use(cors.Handler(cors.Options{
 		AllowedOrigins: []string{"*"},
 	}))
