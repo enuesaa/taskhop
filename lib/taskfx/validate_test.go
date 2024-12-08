0dd492189c654f295498e91310c03c396f3da33f
nua <enuesaa@gmail.com>
2024-12-09 00:01:59 +0900

    feat(taskfx): テストを gomock で書き直した

--- lib/taskfx/validate_test.go
+++ lib/taskfx/validate_test.go
@@ -1,6 +1,7 @@
 package taskfx
 
 import (
+	"strings"
 	"testing"
 
 	"github.com/enuesaa/taskhop/cli"
@@ -8,25 +9,29 @@ import (
 	"github.com/stretchr/testify/assert"
 	"go.uber.org/fx"
 	"go.uber.org/fx/fxtest"
+	"go.uber.org/mock/gomock"
 )
 
 func TestValidate(t *testing.T) {
-	mock := repository.Mock{
-		Cmds: `
+	ctl := gomock.NewController(t)
+	defer ctl.Finish()
+
+	mockRepo := repository.NewMockI(ctl)
+	cmdsyml := strings.NewReader(`
 title: Example
 
 cmds:
   - echo hello!
   - pwd
-`,
-	}
+`)
+	mockRepo.EXPECT().Read("cmds.yml").Return(cmdsyml, nil)
 
 	var i I
 	fxtest.New(
 		t,
 		fx.Supply(
 			fx.Annotate(&cli.Cli{}, fx.As(new(cli.ICli))),
-			fx.Annotate(&mock, fx.As(new(repository.I))),
+			fx.Annotate(mockRepo, fx.As(new(repository.I))),
 		),
 		fx.Provide(New),
 		fx.Populate(&i),
