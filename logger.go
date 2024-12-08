ea4e5582e573c66dc45ba7b079d0e4e37ecacf79
nua <enuesaa@gmail.com>
2024-12-08 23:33:48 +0900

    feat(logger): -debug フラグを与えられた時に詳細なログを出力するようロガーを作成

---
+++ logger.go
@@ -0,0 +1,51 @@
+package main
+
+import (
+	"log"
+
+	"go.uber.org/fx/fxevent"
+)
+
+func NewLogger() fxevent.Logger {
+	return &Logger{
+		Debug: false,
+	}
+}
+
+type Logger struct{
+	Debug bool
+}
+
+func (l *Logger) LogEvent(event fxevent.Event) {
+	if l.Debug {
+		l.logEventDebug(event)
+	} else {
+		l.logEventNormal(event)
+	}
+}
+
+func (l *Logger) logEventDebug(event fxevent.Event) {
+	switch e := event.(type) {
+	case *fxevent.Started:
+		log.Printf("Started: %s\n", e)
+	case *fxevent.Stopping:
+		log.Printf("Stopping: %s\n", e)
+	case *fxevent.Stopped:
+		if e.Err != nil {
+			log.Printf("Error: %s\n", e.Err.Error())
+		}
+	default:
+		log.Printf("Event: %v\n", e)
+	}
+}
+
+func (l *Logger) logEventNormal(event fxevent.Event) {
+	switch e := event.(type) {
+	case *fxevent.Stopped:
+		if e.Err != nil {
+			log.Printf("Error: %s\n", e.Err.Error())
+		}
+	default:
+		// do not log
+	}
+}
--- main.go
+++ main.go
@@ -8,25 +8,29 @@ import (
 	"github.com/enuesaa/taskhop/cli"
 	"github.com/enuesaa/taskhop/lib"
 	"go.uber.org/fx"
+	"go.uber.org/fx/fxevent"
 )
 
 func main() {
+	logger := Logger{
+		Debug: false,
+	}
+
 	app := fx.New(
 		cli.Module,
 		lib.Module,
 		commander.Module,
 		runner.Module,
+		fx.WithLogger(func () fxevent.Logger {
+			return &logger
+		}),
 		fx.Invoke(func (cl cli.ICli) error {
 			return cl.Launch()
 		}),
-		// fx.Invoke(func (cl fx.NopLogger) fx.Option {
-		// 	if cl.IsDebug() {
-		// 		return fx.Options() // TODO: customize logger
-		// 	}
-		// 	return fx.NopLogger
-		// }),
+		fx.Invoke(func (cl cli.ICli) {
+			logger.Debug = cl.IsDebug()
+		}),
 		fx.Invoke(func(cl cli.ICli, commanderApp commander.App, runnerApp runner.App) error {
-			// これ: この辺りで動的に 		fx.NopLogger, を指定したい
 			if cl.IsCommander() {
 				return commanderApp.Run()
 			}
