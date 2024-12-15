13beeea27e2b12606a7d4eeef4ee8c0de035b30a
nua <enuesaa@gmail.com>
2024-12-15 19:47:54 +0900

    fix(gql): `handler.NewDefaultServer is deprecated: This was and is just an example. (SA1019)`

--- app/commander/gql/main.go
+++ app/commander/gql/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/99designs/gqlgen/graphql/handler"
+	"github.com/99designs/gqlgen/graphql/handler/transport"
 	"github.com/enuesaa/taskhop/lib"
 )
 
@@ -11,7 +12,13 @@ func Handle(c lib.Lib) http.HandlerFunc {
 	schema := NewExecutableSchema(Config{
 		Resolvers: &Resolver{c},
 	})
-	handle := handler.NewDefaultServer(schema)
+
+	handle := handler.New(schema)
+
+	handle.AddTransport(transport.Options{})
+	handle.AddTransport(transport.GET{})
+	handle.AddTransport(transport.POST{})
+	handle.AddTransport(transport.MultipartForm{})
 
 	return handle.ServeHTTP
 }
