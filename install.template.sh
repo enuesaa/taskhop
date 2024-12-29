ee08003702c85be825c5066bd47bf41242b5cfa3
nua <enuesaa@gmail.com>
2024-12-29 16:26:27 +0900

    chore(release): create tmp dir and unarchive tar.gz

--- install.template.sh
+++ install.template.sh
@@ -4,7 +4,7 @@
 VERSION="${VERSION}"; # like `v0.0.3`
 
 if [[ "$VERSION" != v* ]]; then
-  echo "Error: Incorrect install script.";
+  echo "Error: Incorrect install script";
   exit 1;
 fi
 
@@ -24,10 +24,24 @@ else
 fi
 
 URL="https://github.com/enuesaa/taskhop/releases/download/v${VERSION_NUMBER}/taskhop_${VERSION_NUMBER}_${OS}_${ARCH}.tar.gz";
+TMP_OUT_DIR="taskhop_${VERSION_NUMBER}_${OS}_${ARCH}";
+
 echo "Downloading $URL";
+echo "And the archive file will be extracted to ${TMP_OUT_DIR}";
 echo "";
 
-curl -L "$URL" | tar -xz -C ~/tmp;
+if [ -d "$TMP_OUT_DIR" ]; then
+  echo "Error: Installation stopped because the directory ${TMP_OUT_DIR} already exists"
+  exit 1;
+fi
+
+mkdir $TMP_OUT_DIR;
+
+# download and install
+curl -L "$URL" | tar -xz -C $TMP_OUT_DIR;
+mv $TMP_OUT_DIR/taskhop /usr/local/bin/taskhop;
+
+rm -rf $TMP_OUT_DIR;
 
 echo "";
 echo "Installation completed!";
