01bfe6f52105432113ee8902e26c6b88cdb22804
nua <enuesaa@gmail.com>
2025-01-08 23:37:35 +0900

    chore(ci): detect missing go generate

--- tools.go
+++ tools.go
@@ -2,11 +2,14 @@
 
 package main
 
-// import dev tools here to lock its version.
+// This file imports dev tools to lock its version.
 
 import (
 	_ "github.com/99designs/gqlgen"
 )
+
+// Run following command to generate code:
+//   go generate tools.go
 
 //go:generate go run github.com/99designs/gqlgen generate
 //go:generate gqlgenc
