264316cd281c66a6afc34a5976df2acbd214919b
nua <enuesaa@gmail.com>
2025-03-11 20:48:13 +0900

    chore: use go 1.24 tool directive

--- go.mod
+++ go.mod
@@ -47,3 +47,8 @@ require (
 	golang.org/x/text v0.23.0 // indirect
 	golang.org/x/tools v0.31.0 // indirect
 )
+
+tool (
+	github.com/99designs/gqlgen
+	github.com/Yamashou/gqlgenc
+)
--- tools.go
+++ tools.go
@@ -1,16 +1,4 @@
-//go:build tools
-
 package main
 
-// This file imports dev tools to lock its version.
-
-import (
-	_ "github.com/99designs/gqlgen"
-	_ "github.com/Yamashou/gqlgenc"
-)
-
-// Run following command to generate code:
-//   go generate tools.go
-
-//go:generate go run github.com/99designs/gqlgen generate
-//go:generate go run github.com/Yamashou/gqlgenc
+//go:generate go tool github.com/99designs/gqlgen generate
+//go:generate go tool github.com/Yamashou/gqlgenc
